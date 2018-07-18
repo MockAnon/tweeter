ceb0ec2f0470a57c4849365ac479020aa755c92b
Jaron D Evans <jarondevans@gmail.com>
2018-07-18 17:57:37 +0000

    commented out Posting older tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -33,43 +33,43 @@ $(function() {
 
 //___________Posting older tweets_________
 
-function createTweetElement(obj){
-  console.log('called');
-  const userName = obj.user.name;
-  const postContent = obj.content.text;
-  const creationTime = obj.created_at;
-  const postAvatar = obj.user.avatars['small'];
-  const handle = obj.user.handle;
+// function createTweetElement(obj){
+//   console.log('called');
+//   const userName = obj.user.name;
+//   const postContent = obj.content.text;
+//   const creationTime = obj.created_at;
+//   const postAvatar = obj.user.avatars['small'];
+//   const handle = obj.user.handle;
 
-$('#tweets-container').append(`
-  <article class="tweet">
-    <header>
-      <img class="thumb" src= ${postAvatar} >
-      <h1> ${userName} </h1>
-      <p> ${handle} </p>
-    </header>
-    <div class="content"> ${postContent} </div>
-    <footer>${creationTime}</footer>
-  </article>`
-  )
+// $('#tweets-container').append(`
+//   <article class="tweet">
+//     <header>
+//       <img class="thumb" src= ${postAvatar} >
+//       <h1> ${userName} </h1>
+//       <p> ${handle} </p>
+//     </header>
+//     <div class="content"> ${postContent} </div>
+//     <footer>${creationTime}</footer>
+//   </article>`
+//   )
 
-}
+// }
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-    },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": {
+//       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+//       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+//       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+//     },
+//     "handle": "@SirIsaac"
+//   },
+//   "content": {
+//     "text": "If I have seen further it is by standing on the shoulders of giants"
+//   },
+//   "created_at": 1461116232227
+// }
 
 //___________RENDERING TWEETS DAY 3_____________
 
@@ -100,5 +100,5 @@ const tweetData = {
   }
 
 //____don't touch below____________
-  createTweetElement(tweetData);
+  // createTweetElement(tweetData);
 });
